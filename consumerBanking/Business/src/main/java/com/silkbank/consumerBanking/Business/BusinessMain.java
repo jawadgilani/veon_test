56d1b0f55cfd9003eea304e16aa68943fa052934
jawadgilani <SyedJawad.Gilani@thinkbiganalytics.com>
2017-07-13 14:09:31 +0500

    Add a Branch.

--- consumerBanking/Business/src/main/java/com/silkbank/consumerBanking/Business/BusinessMain.java
+++ consumerBanking/Business/src/main/java/com/silkbank/consumerBanking/Business/BusinessMain.java
@@ -5,6 +5,8 @@ public class BusinessMain {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hi I am A Business Mondule");
+		
+		System.out.println("Now Checking Branch Concept");
 
 	}
 
