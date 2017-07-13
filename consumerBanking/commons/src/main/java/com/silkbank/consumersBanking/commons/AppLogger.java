4dbfc1617bb3eb3257cb241b45ea3fcf21eb9b1c
jawadgilani <SyedJawad.Gilani@thinkbiganalytics.com>
2017-07-13 16:29:16 +0500

    Third Module Added. Module Name : Commons

---
+++ consumerBanking/commons/src/main/java/com/silkbank/consumersBanking/commons/AppLogger.java
@@ -0,0 +1,10 @@
+package com.silkbank.consumersBanking.commons;
+
+public class AppLogger {
+
+	public static void main(String[] args) {
+		System.out.println("I am commons You can Use me as template");
+
+	}
+
+}
